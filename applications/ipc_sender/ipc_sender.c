efb777f774de8b716800d7d71bac424563899151
Zhihao Yao <z.yao@uci.edu>
2020-07-16 07:57:55 -0700

    a minor fix, the last line of macro shouldn't have backslash

--- applications/ipc_sender/ipc_sender.c
+++ applications/ipc_sender/ipc_sender.c
@@ -19,10 +19,10 @@
 char output_buf[64];
 int num_chars = 0;
 #define secure_printf(fmt, args...) {memset(output_buf, 0x0, 64); sprintf(output_buf, fmt, ##args);	\
-					 api->write_to_secure_serial_out(output_buf);}			\
+					 api->write_to_secure_serial_out(output_buf);}
 
 #define insecure_printf(fmt, args...) {memset(output_buf, 0x0, 64); num_chars = sprintf(output_buf, fmt, ##args);\
-					 api->write_to_shell(output_buf, num_chars);}				 \
+					 api->write_to_shell(output_buf, num_chars);}
 
 #endif
 
