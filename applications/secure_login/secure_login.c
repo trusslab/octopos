c86122be013cc41ee52495a982687070a8b73533
Zhihao Yao <z.yao@uci.edu>
2020-08-06 12:04:31 -0700

    enable secure login app

--- applications/secure_login/secure_login.c
+++ applications/secure_login/secure_login.c
@@ -1,5 +1,3 @@
-#ifndef ARCH_SEC_HW
-
 /* secure_login app */
 #include <stdio.h>
 #include <string.h>
@@ -7,10 +5,17 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <sys/stat.h>
+
+#ifdef ARCH_SEC_HW_RUNTIME
+#include "arch/sec_hw.h"
+#include "arch/app_utilities.h"
+#endif
+
 #include <octopos/runtime.h>
 #include <octopos/storage.h>
 
 /* FIXME: how does the app know the size of the buf? */
+#ifndef ARCH_SEC_HW
 char output_buf[64];
 int num_chars = 0;
 #define secure_printf(fmt, args...) {memset(output_buf, 0x0, 64); sprintf(output_buf, fmt, ##args);	\
@@ -19,8 +24,14 @@ int num_chars = 0;
 #define insecure_printf(fmt, args...) {memset(output_buf, 0x0, 64); num_chars = sprintf(output_buf, fmt, ##args);\
 				     api->write_to_shell(output_buf, num_chars);}				 \
 
+#endif
+
+#ifndef ARCH_SEC_HW
 extern "C" __attribute__ ((visibility ("default")))
 void app_main(struct runtime_api *api)
+#else
+void secure_login(struct runtime_api *api)
+#endif
 {
 	char line[1024];
 	int i, size;
@@ -61,7 +72,11 @@ void app_main(struct runtime_api *api)
 	memset(line, 0x0, 1024);
 	for (i = 0; i < 1024; i++) {
 		api->read_char_from_secure_keyboard(&line[i]);
-		if (line[i] == '\n')
+#ifdef ARCH_SEC_HW
+		if (line[i] == '\r') {
+#else
+		if (line[i] == '\n') {
+#endif
 			break;
 	}
 
@@ -99,4 +114,3 @@ void app_main(struct runtime_api *api)
 	insecure_printf("secret (from secure storage): %s (size = %d)\n", line, size);	
 	api->delete_and_yield_secure_storage();
 }
-#endif
