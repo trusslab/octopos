9a5180895dd695f008d66f10c6402bee4769b287
Ardalan Amiri Sani <arrdalan@gmail.com>
2020-09-11 14:10:49 -0700

    Fix a compilation error in untrusted linux (umode).

--- include/octopos/syscall.h
+++ include/octopos/syscall.h
@@ -6,12 +6,8 @@
 #ifndef UNTRUSTED_DOMAIN
 #include "arch/syscall.h"
 #else
-/* FIXME: move somewhere else */
-#define SERIALIZE_16(arg, buf_lr)				\
-	*((uint16_t *) buf_lr) = arg;				
+#include <octopos/syscall_umode.h>
 
-#define SERIALIZE_32(arg, buf_lr)				\
-	*((uint32_t *) buf_lr) = arg;	
 #define printf printk
 #endif
 
