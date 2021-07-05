bdac1b95bec2f4e661000fbc6bdc090a87bcb275
Zhihao Yao <z.yao@uci.edu>
2021-07-05 09:08:54 -0700

    revert debug changes

--- applications/fs_test/fs_test.c
+++ applications/fs_test/fs_test.c
@@ -73,35 +73,35 @@ void fs_test(struct runtime_api *api)
 	}
 
 	insecure_printf("Test 1 passed.\n");
-	// insecure_printf("Test 2\n");
-	// uint8_t block[STORAGE_BLOCK_SIZE * 100];
-	// memset(block, 0x0, STORAGE_BLOCK_SIZE);
+	insecure_printf("Test 2\n");
+	uint8_t block[STORAGE_BLOCK_SIZE * 100];
+	memset(block, 0x0, STORAGE_BLOCK_SIZE);
 
-	// block[10] = 14;
-	// // api->write_file_blocks(fd2, block, 0, 1);
-	// memset(block, 0x0, STORAGE_BLOCK_SIZE);
-	// api->read_file_blocks(fd2, block, 0, 1);
-	// insecure_printf("block[10] = %d\n", (int) block[10]);
-	// if (block[10] != 14) {
-	// 	insecure_printf("Test 2 failed\n");
-	// 	goto out;
-	// }
-	// insecure_printf("Test 2 passed.\n");
+	block[10] = 14;
+	api->write_file_blocks(fd2, block, 0, 1);
+	memset(block, 0x0, STORAGE_BLOCK_SIZE);
+	api->read_file_blocks(fd2, block, 0, 1);
+	insecure_printf("block[10] = %d\n", (int) block[10]);
+	if (block[10] != 14) {
+		insecure_printf("Test 2 failed\n");
+		goto out;
+	}
+	insecure_printf("Test 2 passed.\n");
 
-	// insecure_printf("Test 3\n");
-	// memset(block, 0x0, STORAGE_BLOCK_SIZE * 100);
+	insecure_printf("Test 3\n");
+	memset(block, 0x0, STORAGE_BLOCK_SIZE * 100);
 
-	// index = (99 * STORAGE_BLOCK_SIZE) + 10;
-	// block[index] = 12;
-	// // api->write_file_blocks(fd2, block, 1, 100);
-	// memset(block, 0x0, STORAGE_BLOCK_SIZE * 100);
-	// api->read_file_blocks(fd2, block, 1, 100);
-	// insecure_printf("block[index] = %d\n", (int) block[index]);
-	// if (block[index] != 12) {
-	// 	insecure_printf("Test 3 failed\n");
-	// 	goto out;
-	// }
-	// insecure_printf("Test 3 passed.\n");
+	index = (99 * STORAGE_BLOCK_SIZE) + 10;
+	block[index] = 12;
+	api->write_file_blocks(fd2, block, 1, 100);
+	memset(block, 0x0, STORAGE_BLOCK_SIZE * 100);
+	api->read_file_blocks(fd2, block, 1, 100);
+	insecure_printf("block[index] = %d\n", (int) block[index]);
+	if (block[index] != 12) {
+		insecure_printf("Test 3 failed\n");
+		goto out;
+	}
+	insecure_printf("Test 3 passed.\n");
 
 out:
 	api->close_file(fd1);
