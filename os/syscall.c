e3e426581ea7fd1d7b1494fccfc0fb189f415d23
Ardalan Amiri Sani <arrdalan@gmail.com>
2021-11-26 11:30:06 -0800

    Revert "Verify that the keyboard domain is reset before delegation."
    This reverts commit 5dbde1e5270cd41cba0c6ead9e90371e4d53aa5b.
    
    Rationale: with our current threat model (i.e., no physical attacks),
    the keyboard domain does not receive any untrusted inputs. Therefore, it
    is weakly trusted even without resetting.

--- os/syscall.c
+++ os/syscall.c
@@ -177,7 +177,7 @@ static void handle_syscall(uint8_t runtime_proc_id, uint8_t *buf,
 
 		mark_queue_unavailable(Q_SERIAL_OUT);
 
-		/* reset the serial_out domain before delegation */
+		/* reset serial_out domain before delegation */
 		reset_proc(P_SERIAL_OUT);
 
 		mailbox_delegate_queue_access(Q_SERIAL_OUT, runtime_proc_id,
@@ -214,9 +214,6 @@ static void handle_syscall(uint8_t runtime_proc_id, uint8_t *buf,
 
 		mark_queue_unavailable(Q_KEYBOARD);
 
-		/* reset the keyboard domain before delegation */
-		reset_proc(P_KEYBOARD);
-		
 		mailbox_delegate_queue_access(Q_KEYBOARD, runtime_proc_id,
 					      (limit_t) limit,
 					      (timeout_t) timeout);
