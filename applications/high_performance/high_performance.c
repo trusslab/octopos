f60059faa2ea6f2a3a0e2e6b06c47c9015010ffc
Mingyi Chen <mingyic4@uci.edu>
2022-02-27 20:02:14 -0800

    Initial of app

---
+++ applications/high_performance/high_performance.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <octopos/runtime.h>
+
+char output_buf[64];
+int num_chars = 0;
+#define secure_printf(fmt, args...) {memset(output_buf, 0x0, 64); sprintf(output_buf, fmt, ##args);	\
+					 api->write_to_secure_serial_out(output_buf);}
+
+#define insecure_printf(fmt, args...) {memset(output_buf, 0x0, 64); num_chars = sprintf(output_buf, fmt, ##args);\
+					 api->write_to_shell(output_buf, num_chars);}
+
+void *senderThread(struct runtime_api *api)
+{
+	char line[1024];
+	int size;
+	uint8_t target_qid = 0;
+	uint8_t own_qid = api->get_runtime_queue_id();
+
+	/* send message */
+	insecure_printf("%c", own_qid);
+
+	/* receive response */
+	int ret = api->read_from_shell(line, &size);
+	if (ret || size == 0) {
+		printf("Didn't receive a valid response\n");
+		return;
+	}
+
+	target_qid = line[0];
+	printf("Received response: target_qid = %d (size = %d)\n", target_qid, size);
+	/* secure IPC */
+	ret = api->request_secure_ipc(target_qid, 200, 100, NULL);
+	if (ret) {
+		printf("Couldn't establish secure IPC (ret = %d)\n", ret);
+		return;
+	}
+
+	char secure_msg[64] = "secure msg 1";
+	int secure_msg_size = sizeof("secure msg 1");
+	api->send_msg_on_secure_ipc(secure_msg, secure_msg_size);
+
+	api->recv_msg_on_secure_ipc(secure_msg, &secure_msg_size);
+	printf("Received secure msg: %s (size = %d)\n", secure_msg, secure_msg_size);
+
+	api->yield_secure_ipc();
+}
+
+void *receiveThread(struct runtime_api *api)
+{
+	char line[1024];
+	int size;
+	uint8_t target_qid = 0;
+	uint8_t own_qid = api->get_runtime_queue_id();
+
+	/* send message */
+	insecure_printf("%c", own_qid);
+
+	/* receive response */
+	int ret = api->read_from_shell(line, &size);
+	if (ret || size == 0) {
+		printf("Didn't receive a valid response\n");
+		return;
+	}
+
+	target_qid = line[0];
+	printf("Received response: target_qid = %d (size = %d)\n", target_qid, size);
+
+	/* secure IPC */
+	ret = api->request_secure_ipc(target_qid, 200, 100, NULL);
+	if (ret) {
+		printf("Couldn't establish secure IPC (ret = %d)\n", ret);
+		return;
+	}
+
+	char secure_msg[64] = "secure msg 1";
+	int secure_msg_size = sizeof("secure msg 1");
+	api->send_msg_on_secure_ipc(secure_msg, secure_msg_size);
+
+	api->recv_msg_on_secure_ipc(secure_msg, &secure_msg_size);
+	printf("Received secure msg: %s (size = %d)\n", secure_msg, secure_msg_size);
+
+	api->yield_secure_ipc();
+}
+
+extern "C" __attribute__ ((visibility ("default")))
+void app_main(struct runtime_api *api)
+{
+	pthread_t sender_thread, receiver_thread;
+	pthread_create(&sender_thread, NULL, sender_thread, api);
+	phtread_create(&receiver_thread, NULL, receiver_thread, api);
+
+	pthread_exit(NULL);
+}
